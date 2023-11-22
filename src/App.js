fca43f66c6f9f395dd37ea130eba4b698b3927bc
Felipe Nascimento <felipe@on2.dev>
2023-11-21 23:59:02 -0300

    added placeholder

--- src/App.js
+++ src/App.js
@@ -138,7 +138,7 @@ export default function App() {
     <div className="App">
       <div className="header">
         <select onChange={onStateChange}>
-          <option value=""></option>
+          <option value="">Selecione um estado</option>
           {statesList.sort(sortByName).map((state) => {
             return (
               <option key={state.sigla} value={state.sigla}>
